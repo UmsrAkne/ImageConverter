93dec1328efb61c695e52943b4a0892f1162a71f
Akn <sibata486@gmail.com>
2024-01-03 15:10:04 +0900

    init

---
+++ ImageConverter/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using ImageConverter.Views;
+using Prism.Ioc;
+
+namespace ImageConverter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ ImageConverter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Prism.Mvvm;
+
+namespace ImageConverter.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Image Converter";
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+}
