d0da2fc0ff997205e1ff18410712d743a0aa4fa3
Akn <sibata486@gmail.com>
2024-02-05 22:32:50 +0900

    add file / 画像変換処理のタイプを表す Enum を追加

---
+++ ImageConverter/Models/ProcessType.cs
@@ -0,0 +1,15 @@
+namespace ImageConverter.Models
+{
+    public enum ProcessType
+    {
+        /// <summary>
+        ///     bmp から png への画像変換を表しています。
+        /// </summary>
+        BmpToPng,
+
+        /// <summary>
+        ///     webp から png への画像変換を表しています。
+        /// </summary>
+        WebpToPng,
+    }
+}
